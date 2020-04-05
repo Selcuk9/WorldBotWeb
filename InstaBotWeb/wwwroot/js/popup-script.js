899fc6b1d0beb4fe1168a04ca60e253c6c841646
Selcuk Ilyazov <ilyazov49@gmail.com>
2020-04-06 02:55:12 +0300

    Update main page

---
+++ InstaBotWeb/wwwroot/js/popup-script.js
@@ -0,0 +1,22 @@
+﻿let more = document.getElementById('more-info'),
+    modal = document.querySelector('.fixed-overlay'),
+    btnClose = document.querySelector('.popup-close');
+
+
+more.addEventListener('click', function () {
+    showModal();
+});
+btnClose.addEventListener('click', function () {
+    closeModal();
+});
+modal.addEventListener('click', function (event) {
+    closeModal(event);
+});
+
+function showModal() {
+    modal.style.display = "block";
+}
+
+function closeModal(event) {
+    modal.style.display = "none";
+}
